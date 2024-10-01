ea58b54ead01ae2e3ea59741d54a0227174e0b6e
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-10-01 20:03:22 -0300

    fix vercel 6

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,12 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/*',
-          dest: 'assets'
-        }
-      ]
-    })
   ],
   resolve: {
     alias: {
@@ -24,4 +15,5 @@ export default defineConfig({
     },
   },
   base: './',
+  publicDir: 'public',
 });
